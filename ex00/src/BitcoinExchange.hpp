98e2719b859ddeea58735a9a65fa7accc0cc55b0
tachu <roatyungtatyu@gmail.com>
2024-10-24 13:56:55 +0900

    add: init files

---
+++ ex00/src/BitcoinExchange.hpp
@@ -0,0 +1,14 @@
+#ifndef BITCOIN_EXCHANGE_HPP
+#define BITCOIN_EXCHANGE_HPP
+
+class BitcoinExchange {
+public:
+  BitcoinExchange();
+  BitcoinExchange(const BitcoinExchange &);
+  BitcoinExchange &operator=(const BitcoinExchange &);
+  ~BitcoinExchange();
+
+private:
+};
+
+#endif // !BITCOIN_EXCHANGE_HPP
