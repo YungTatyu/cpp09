f44f06d5e77e7b46eb5dc20b7cb36c3b2191b30b
tachu <roatyungtatyu@gmail.com>
2024-10-25 13:05:51 +0900

    design: in progress

--- ex00/src/BitcoinExchange.hpp
+++ ex00/src/BitcoinExchange.hpp
@@ -1,14 +1,30 @@
 #ifndef BITCOIN_EXCHANGE_HPP
 #define BITCOIN_EXCHANGE_HPP
 
+#include <string>
+
 class BitcoinExchange {
 public:
+  BitcoinExchange(const std::string &);
+  ~BitcoinExchange();
+
+  std::string rate_database_;
+  std::string price_database_;
+
+private:
   BitcoinExchange();
   BitcoinExchange(const BitcoinExchange &);
   BitcoinExchange &operator=(const BitcoinExchange &);
-  ~BitcoinExchange();
+  bool parseRateDatabase();
+  bool parsePriceDatabase();
+  bool isDate(const std::string &) const;
+  bool isValidPrice(const std::string &) const;
+  double findClosetPrice() const;
+  double findClosetRate() const;
 
-private:
+  static const char *rate_database_file_ = "";
+  static const int kMinPrice_ = 0;
+  static const int kMaxPrice_ = 1000;
 };
 
 #endif // !BITCOIN_EXCHANGE_HPP
