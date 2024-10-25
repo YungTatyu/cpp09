ad1e83a696c6773afbfd7b10f6115e3f035cf875
tachu <roatyungtatyu@gmail.com>
2024-10-25 14:36:20 +0900

    add: struct Date

--- ex00/src/BitcoinExchange.hpp
+++ ex00/src/BitcoinExchange.hpp
@@ -1,15 +1,43 @@
 #ifndef BITCOIN_EXCHANGE_HPP
 #define BITCOIN_EXCHANGE_HPP
 
+#include <map>
 #include <string>
+
+struct Date {
+  Date(int year, int month, int day) {
+    year_ = year;
+    month_ = month;
+    day_ = day;
+  }
+  Date(const Date &other) { *this = other; }
+  ~Date() {}
+  Date &operator=(const Date &other) {
+    if (this != &other) {
+      year_ = other.year_;
+      month_ = other.month_;
+      day_ = other.day_;
+    }
+    return *this;
+  }
+
+  int year_;
+  int month_;
+  int day_;
+
+private:
+  Date();
+};
 
 class BitcoinExchange {
 public:
   BitcoinExchange(const std::string &);
   ~BitcoinExchange();
 
-  std::string rate_database_;
-  std::string price_database_;
+  std::string raw_rate_data_;
+  std::string raw_price_data_;
+  std::map<Date, double> rate_map_;
+  std::map<Date, double> price_map_;
 
 private:
   BitcoinExchange();
@@ -22,7 +50,7 @@ private:
   double findClosetPrice() const;
   double findClosetRate() const;
 
-  static const char *rate_database_file_ = "";
+  static const char *kRateDatabaseFile_ = "";
   static const int kMinPrice_ = 0;
   static const int kMaxPrice_ = 1000;
 };
