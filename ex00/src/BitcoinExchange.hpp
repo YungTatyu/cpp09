f4c5ea2b6749bd5ed3e5eb8245e2479ac575cdcd
tachu <roatyungtatyu@gmail.com>
2024-10-29 14:29:13 +0900

    modify: BitcoinExchange design

--- ex00/src/BitcoinExchange.hpp
+++ ex00/src/BitcoinExchange.hpp
@@ -3,6 +3,7 @@
 
 #include <map>
 #include <string>
+#include <utility>
 
 struct Date {
   Date(int year, int month, int day) {
@@ -20,6 +21,15 @@ struct Date {
     }
     return *this;
   }
+  bool operator<(const Date &other) const {
+    if (year_ != other.year_) {
+      return year_ < other.year_;
+    }
+    if (month_ != other.month_) {
+      return month_ < other.month_;
+    }
+    return day_ < other.day_;
+  }
 
   int year_;
   int month_;
@@ -31,30 +41,28 @@ private:
 
 class BitcoinExchange {
 public:
+  BitcoinExchange();
   BitcoinExchange(const std::string &);
+  BitcoinExchange(const BitcoinExchange &);
   ~BitcoinExchange();
-  bool run();
+  BitcoinExchange &operator=(const BitcoinExchange &);
+  bool ProcessFileForCalculation(const std::string &);
+  bool ProcessLineForCalculation(const std::string &);
 
   std::string raw_rate_data_;
-  std::string raw_price_data_;
+  std::string raw_price_data_; // use this when parsing price file
   std::map<Date, double> rate_map_;
-  std::map<Date, double> price_map_;
 
 private:
-  BitcoinExchange();
-  BitcoinExchange(const BitcoinExchange &);
-  BitcoinExchange &operator=(const BitcoinExchange &);
-  bool setup();
-  bool parseRateData();
-  bool parsePriceData();
-  bool parseDate(const std::string &, Date &) const;
-  bool isValidPrice(const std::string &) const;
-  double findClosetPrice(const Date &) const;
-  double findClosetRate(const Date &) const;
-  double calcValue(const Date &) const;
-  std::string readFile(const std::string &) const;
+  bool ParseRateData();
+  std::pair<Date, double> ParsePriceLine(const std::string &);
+  Date ParseDate(const std::string &) const;
+  double ParsePrice(const std::string &) const;
+  double FindClosetRate(const Date &) const;
+  double CalcValue(const Date &) const;
+  std::string ReadFile(const std::string &) const;
 
-  static const char *kRateDataFile_;
+  static const char *kDefaultRateDataFile_;
   static const int kMinPrice_ = 0;
   static const int kMaxPrice_ = 1000;
 };
