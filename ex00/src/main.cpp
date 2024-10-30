be6ff3c4536cb40f2013c1d5d0e89e009a4f8586
tachu <roatyungtatyu@gmail.com>
2024-10-30 15:54:57 +0900

    modify: main so its easy to test

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -3,9 +3,17 @@
 
 #include "BitcoinExchange.hpp"
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "usuage: " << argv[0] << " <database>" << std::endl;
+  if (argc != 2 && argc != 3) {
+    std::cerr << "usuage: " << argv[0]
+              << " database for price [database for rate]" << std::endl;
     return 1;
+  }
+  if (argc == 3) {
+    BitcoinExchange bc(argv[2]);
+    if (!bc.ProcessFileForCalculation(argv[1])) {
+      return 1;
+    }
+    return 0;
   }
   BitcoinExchange bc;
   if (!bc.ProcessFileForCalculation(argv[1])) {
