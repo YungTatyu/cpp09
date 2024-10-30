9248471163db5e704d3e06fdff593e869fa5bcc2
tachu <roatyungtatyu@gmail.com>
2024-10-30 13:54:48 +0900

    modify: update main()

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -4,10 +4,12 @@
 #include "BitcoinExchange.hpp"
 int main(int argc, char *argv[]) {
   if (argc != 2) {
-    std::cerr << "usuage: " << argv[0] << "<database>";
+    std::cerr << "usuage: " << argv[0] << " <database>" << std::endl;
     return 1;
   }
-  BitcoinExchange bc(argv[1]);
-
+  BitcoinExchange bc;
+  if (!bc.ProcessFileForCalculation(argv[1])) {
+    return 1;
+  }
   return 0;
 }
